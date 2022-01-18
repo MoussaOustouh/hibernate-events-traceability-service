4c7fcffd98977f0c2d6c2cd355d3eaf9529e220f
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2022-01-18 15:37:47 +0100

    first commit

---
+++ src/main/java/mo/spring/hibernateeventstraceabilityservice/HibernateEventsTraceabilityServiceApplication.java
@@ -0,0 +1,13 @@
+package mo.spring.hibernateeventstraceabilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEventsTraceabilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateEventsTraceabilityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mo/spring/hibernateeventstraceabilityservice/HibernateEventsTraceabilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mo.spring.hibernateeventstraceabilityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateEventsTraceabilityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
